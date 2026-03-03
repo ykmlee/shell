7812c9946fb7815eab40114a0ad592c1ee5836e0
Yvonne Lee <yvonneleekm@gmail.com>
2026-03-03 16:36:09 -0500

    another revision 2

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -58,9 +58,9 @@ rm data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch data/inventory.txt
-ls data/processed/server_logs >> inventory.txt
-ls data/processed/user_logs >> inventory.txt
-ls data/processed/event_logs >> inventory.txt
+ls data/processed/server_logs >> data/inventory.txt
+ls data/processed/user_logs >> data/inventory.txt
+ls data/processed/event_logs >> data/inventory.txt
 
 
 ###########################################
